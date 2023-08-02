ac151b7318869fa2663ff41fff712e8ec00f44ab
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-02 14:37:07 -0700

    Updated alu test #1 to include flags

--- src/tests/alu_testbench.cpp
+++ src/tests/alu_testbench.cpp
@@ -7,14 +7,15 @@ int main(int argc, char** argv) {
   Valu* top = new Valu;
 
   // Test #1: 1 + 1
-  top -> alu_control = 0b000;
-  top -> A = 0x0000'0001;
-  top -> B = 0x0000'0001;
-  top -> eval();
-  if (top -> result != 0x0000'0002) 
+  top->alu_control = 0b000;
+  top->A = 0x0000'0001;
+  top->B = 0x0000'0001;
+  top->eval();
+  if (top->result != 0x0000'0002 || top->C != 0 || top->V != 0 || top->N !=0 || top->Z != 0) 
   {
     std::cout << "ERROR TEST #1" << std::endl;
     std::cout << "RESULT: " << top -> result << std::endl;
+    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   }
