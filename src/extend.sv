4be1038299e140c1348c0c971e2676a6697a2a6d
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-14 00:10:59 -0700

    Fixed bug in exted.sv

--- src/extend.sv
+++ src/extend.sv
@@ -5,16 +5,16 @@ module extend(input logic [31:7] instruction,
   always_comb
     case(imm_src)
       // I-type
-      2'b00: imm_ext = { {20{instruction[31]}}, instruction[31:20] };
+      2'b00: imm_ext = {{20{instruction[31]}}, instruction[31:20] };
       // S-type
-      2'b01: imm_ext = { {20{instruction[31]}}, instruction[31:25], 
-        instruction[11:7] };
+      2'b01: imm_ext = {{20{instruction[31]}}, instruction[31:25], 
+                        instruction[11:7]};
       // B-type
-      2'b10: imm_ext = { {20{instruction[21}}, instruction[7],
-        instruction[30:25], instruction[11:8], 1'b0} };
+      2'b10: imm_ext = {{20{instruction[31]}}, instruction[7],
+                        instruction[30:25], instruction[11:8], 1'b0};
       // J-type
-      2'b11: imm_ext = { {12{instruction[31]}}, instruction[19:12], 
-        instruction[20], instruction[30:21], 1'b0 };
+      2'b11: imm_ext = {{12{instruction[31]}}, instruction[19:12], 
+                        instruction[20], instruction[30:21], 1'b0};
 
       default: imm_ext = 32'bx;
 
