7e3885cd1823b213a1af25bc8dd57f5553d3c623
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-13 23:41:34 -0700

    Updated dmem to index by [7:2]

--- src/dmem.sv
+++ src/dmem.sv
@@ -3,11 +3,10 @@ module dmem(input logic clk, we,
             output logic [31:0] rd);
 
   logic [31:0] RAM[63:0];
-  assign rd = RAM[a[31:2]];
+  assign rd = RAM[a[7:2]];
 
   always_ff @(posedge clk)
-    if (we)
-      RAM[a[31:2]] <= wd;
+    if (we) RAM[a[7:2]] <= wd;
 
 endmodule
 
