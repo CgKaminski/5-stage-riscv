557bbd0e6b6594a67e9133f623626d0cbca5267d
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-13 23:43:27 -0700

    Updated top to use machine code insted of assembly :/

--- src/top.sv
+++ src/top.sv
@@ -5,11 +5,14 @@ module top(input logic clk, reset,
   logic [31:0] pc, instruction, read_data;
 
   // instantiate processor and memories
-  riscv rvsingle(clk, reset, pc, instruction, mem_write, data_adr,
-                 write_data, read_data);
+  riscv_single_cycle riscv(.clk(clk), .reset(reset), .pc(pc), 
+                           .instruction(instruction), .mem_write(mem_write), 
+                           .alu_result(data_adr), .write_data(write_data), 
+                           .read_data(read_data));
 
-  imem imem(pc, instruction);
-  dmem dmem(clk, mem_write, data_adr, write_data, read_data);
+  imem imem(.a(pc), .rd(instruction));
+  dmem dmem(.clk(clk), .we(mem_write), .a(data_adr), .wd(write_data), 
+            .rd(read_data));
 
 endmodule 
 
