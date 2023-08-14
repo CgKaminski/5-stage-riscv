dab816699110144f584844ef2d753c52b764f683
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-13 23:43:41 -0700

    Update; included implicit parameters

--- src/controller.sv
+++ src/controller.sv
@@ -8,9 +8,13 @@ module controller(input logic [6:0] op,
   logic [1:0] alu_op;
   logic branch;
 
-  main_decoder main_dec(op, result_src, mem_write, branch, alu_src, reg_write,
-                        jump, imm_src, alu_op);
-  alu_decoder alu_dec(op[5], funct3, funct7b5, alu_op, alu_control);
+  main_decoder main_dec(.op(op), .result_src(result_src), .mem_write(mem_write),
+                        .branch(branch), .alu_src(alu_src), 
+                        .reg_write(reg_write), .jump(jump), .imm_src(imm_src), 
+                        .alu_op(alu_op));
+
+  alu_decoder alu_dec(.opb5(op[5]), .funct3(funct3), .funct7b5(funct7b5), 
+                      .alu_op(alu_op), .alu_control(alu_control));
 
   assign pc_src = branch & zero | jump;
 
