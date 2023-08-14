d4c385e80fa885111ec2458314231bf59673cfef
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-13 23:38:36 -0700

    Update datapath.sv to have implicit parameters

--- src/datapath.sv
+++ src/datapath.sv
@@ -1,8 +1,8 @@
 module datapath(input logic clk, reset, pc_src, alu_src, reg_write, 
                 input logic [1:0] result_src, imm_src, 
                 input logic [2:0] alu_control,
-                input logic [31:0] pc, read_data,
-                output logic zero, 
+                input logic [31:0] read_data, instruction,
+                output logic zero, negative, carry, overflow,
                 output logic [31:0] pc, alu_result, write_data);
 
   logic [31:0] pc_next, pc_plus_4, pc_target;
@@ -11,24 +11,21 @@ module datapath(input logic clk, reset, pc_src, alu_src, reg_write,
   logic [31:0] result;
 
   // next PC logic
-  flopr pc_reg(clk, reset, pc_next, pc);
-  pc_plus_4 = pc + 4;
-  pc_target = pc + imm_exit;
-  pc_next = (pc_src == 0) ? pc_plus_4 : pc_target;
+  flopr #(32) pc_reg(.clk(clk), .reset(reset), .d(pc_next), .q(pc));
+  assign pc_plus_4 = pc + 4;
+  assign pc_target = pc + imm_exit;
+  assign pc_next = (pc_src == 0) ? pc_plus_4 : pc_target;
 
   // register file logic
-  rf regfile(clk, reg_write, intruction[19:15], instruction[24:20], 
-    instruction[11:7], result, src_a, write_data);
-  imm_exit = {instruction[31:7], 2'b0000000};
+  regfile regfile(clk, reg_write, instruction[19:15], instruction[24:20], 
+                  instruction[11:7], result, src_a, write_data);
+  assign imm_exit = {instruction[31:7], 7'b0000000};
 
   // alu logic
-  src_b = (write_data == 0) ? imm_exit : alu_src;
-  alu alu(src_a, src_b, alu_control, alu_result, zero);
-  case(result_src):
-    2'b00: result = alu_result;
-    2'b01: result = read_data;
-    2'b10: result = pc_plus_4;
-  endcase 
+  assign src_b = (alu_src == 0) ? write_data : imm_exit;
+  alu alu(.A(src_a), .B(src_b), .alu_control(alu_control), .result(alu_result),
+          .Z(zero), .N(negative), .C(carry), .V(overflow));
+  assign result = result_src[1] ? pc_plus_4 : (result_src[0] ? read_data : alu_result);
 
 endmodule
 
