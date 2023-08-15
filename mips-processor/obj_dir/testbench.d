9e954236d8a927889608d75dbd86b992530929e9
Cameron G. Kaminski <cameron.g.kaminski@gmail.com>
2023-08-15 14:18:37 -0700

    Delete mips-processor/obj_dir directory
    Delete verilator files.

--- mips-processor/obj_dir/testbench.d
+++
@@ -1,7 +0,0 @@
-testbench.o: ../testbench.cpp \
-  /usr/local/Cellar/verilator/5.012/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/5.012/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/5.012/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/5.012/share/verilator/include/verilated_types.h \
-  /usr/local/Cellar/verilator/5.012/share/verilator/include/verilated_funcs.h \
-  Vmips.h
