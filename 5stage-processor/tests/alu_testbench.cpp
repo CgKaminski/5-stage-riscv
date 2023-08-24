5633bb5461cf0b335d26b19b5a465f6a42a42077
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-23 21:27:36 -0700

    Updated alu wires to new names

--- 5stage-processor/tests/alu_testbench.cpp
+++ 5stage-processor/tests/alu_testbench.cpp
@@ -7,99 +7,79 @@ int main(int argc, char** argv) {
   Valu* top = new Valu;
 
   // Test #1: 1 + 1
-  top->alu_control = 0b000;
-  top->A = 0x0000'0001;
-  top->B = 0x0000'0001;
+  top->alu_control_i = 0b000;
+  top->src_a_i = 0x0000'0001;
+  top->src_b_i = 0x0000'0001;
   top->eval();
-  if (top->result != 0x0000'0002 || top->C != 0 || top->V != 0 || top->N != 0 || top->Z != 0) 
+  if (top->result_o != 0x0000'0002 || top->carry_o != 0 || top->overflow_o != 0 || top->negative_o != 0 || top->zero_o != 0) 
   {
     std::cout << "ERROR TEST #1" << std::endl;
-    std::cout << "RESULT: " << top -> result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   }
 
   // Test #2: 1 - 1
-  top->alu_control = 0b001;
-  top->A = 0x0000'0001;
-  top->B = 0x0000'0001;
+  top->alu_control_i = 0b001;
+  top->src_a_i = 0x0000'0001;
+  top->src_b_i = 0x0000'0001;
   top->eval();
-  if (top->result != 0x0000'0000 || top->C != 0 || top->V != 0 || top->N != 0 || top->Z != 1) 
+  if (top->result_o != 0x0000'0000 || top->carry_o != 0 || top->overflow_o != 0 || top->negative_o != 0 || top->zero_o != 1) 
   {
-    std::cout << "ERROR TEST #2" << std::endl;
-    std::cout << "RESULT: " << top -> result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   }
 
   // Test #3: 0x5555'5555 AND 0xAAAA'AAAA
-  top->alu_control = 0b010;
-  top->A = 0x5555'5555;
-  top->B = 0xAAAA'AAAA;
+  top->alu_control_i = 0b010;
+  top->src_a_i = 0x5555'5555;
+  top->src_b_i = 0xAAAA'AAAA;
   top->eval();
-  if (top->result != 0x0000'0000 || top->C != 0 || top->V != 0 || top->N != 0 || top->Z != 1) 
+  if (top->result_o != 0x0000'0000 || top->carry_o != 0 || top->overflow_o != 0 || top->negative_o != 0 || top->zero_o != 1) 
   {
-    std::cout << "ERROR TEST #3" << std::endl;
-    std::cout << "RESULT: " << top->result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   } 
 
   // Test #4: 0x5555'5555 OR 0xAAAA'AAAA
-  top->alu_control = 0b011;
-  top->A = 0x5555'5555;
-  top->B = 0xAAAA'AAAA;
+  top->alu_control_i = 0b011;
+  top->src_a_i = 0x5555'5555;
+  top->src_b_i = 0xAAAA'AAAA;
   top->eval();
-  if (top->result != 0xFFFF'FFFF || top->C != 0 || top->V != 0 || top->N != 1 || top->Z != 0) 
+  if (top->result_o != 0xFFFF'FFFF || top->carry_o != 0 || top->overflow_o != 0 || top->negative_o != 1 || top->zero_o != 0) 
   {
-    std::cout << "ERROR TEST #4" << std::endl;
-    std::cout << "RESULT: " << top->result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   } 
 
   // Test #5 0x7FFF'FFFF + 0x7FFF'FFFF
-  top->alu_control = 0b000;
-  top->A = 0x7FFF'FFFF;
-  top->B = 0x7FFF'FFFF;
+  top->alu_control_i = 0b000;
+  top->src_a_i = 0x7FFF'FFFF;
+  top->src_b_i = 0x7FFF'FFFF;
   top->eval();
-  if (top->result != 0xFFFF'FFFE || top->C != 0 || top->V != 1 || top->N != 1 || top->Z != 0) 
+  if (top->result_o != 0xFFFF'FFFE || top->carry_o != 0 || top->overflow_o != 1 || top->negative_o != 1 || top->zero_o != 0) 
   {
-    std::cout << "ERROR TEST #5" << std::endl;
-    std::cout << "RESULT: " << top->result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   } 
 
   // Test #6 0xFFFF'FFFF - 0x0000'0001
-  top->alu_control = 0b000;
-  top->A = 0xFFFF'FFFF;
-  top->B = 0x0000'0001;
+  top->alu_control_i = 0b000;
+  top->src_a_i = 0xFFFF'FFFF;
+  top->src_b_i = 0x0000'0001;
   top->eval();
-  if (top->result != 0x0000'0000 || top->C != 1 || top->V != 0 || top->N != 0 || top->Z != 1) 
+  if (top->result_o != 0x0000'0000 || top->carry_o != 1 || top->overflow_o != 0 || top->negative_o != 0 || top->zero_o != 1) 
   {
-    std::cout << "ERROR TEST #6" << std::endl;
-    std::cout << "RESULT: " << top->result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   } 
 
   // Test #7: SLT 0x0000'0001 -> 0x0000'0002
-  top->alu_control = 0b101;
-  top->A = 0x0000'0002;
-  top->B = 0x0000'0002;
+  top->alu_control_i = 0b101;
+  top->src_a_i = 0x0000'0002;
+  top->src_b_i = 0x0000'0002;
   top->eval();
-  if (top->result != 0x0000'00000|| top->C != 0 || top->V != 0 || top->N != 0 || top->Z != 1) 
+  if (top->result_o != 0x0000'00000|| top->carry_o != 0 || top->overflow_o != 0 || top->negative_o != 0 || top->zero_o != 1) 
   {
-    std::cout << "ERROR TEST #7" << std::endl;
-    std::cout << "RESULT: " << top->result << std::endl;
-    std::cout << "C: " << top->C << " V: " << top->V << " N: " << top->N << " Z: " << top->Z << std::endl;
     delete top;
     return 1;
   }
