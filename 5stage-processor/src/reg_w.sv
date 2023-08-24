a1f6464698da80e5b8c6f7b151ff3e3eb6754a8e
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-23 22:58:15 -0700

    Updated

--- 5stage-processor/src/reg_w.sv
+++ 5stage-processor/src/reg_w.sv
@@ -1,16 +1,18 @@
 /* 
  * Register file for Memory -> Writeback stage
  */
-module reg_w(input  logic        reg_write_m_i,
-             input  logic [1:0]  result_src_m_i,
-             input  logic [31:0] alu_result_m_i,
-             input  logic [31:0] rd1_m_i,
-             input  logic [4:0]  rd_m_i,
-             input  logic [31:0] pc_plus4_m_i,
-             output logic [31:0] alu_result_w_o,
-             output logic [31:0] rd1_w_o,
-             output logic [4:0]  rd_w_o,
-             output logic [31:0] pc_plus4_w_o);
+module reg_w(
+  input  logic        clk_i,   
+  input  logic        reg_write_m_i,
+  input  logic [1:0]  result_src_m_i,
+  input  logic [31:0] alu_result_m_i,
+  input  logic [31:0] rd1_m_i,
+  input  logic [4:0]  rd_m_i,
+  input  logic [31:0] pc_plus4_m_i,
+  output logic [31:0] alu_result_w_o,
+  output logic [31:0] rd1_w_o,
+  output logic [4:0]  rd_w_o,
+  output logic [31:0] pc_plus4_w_o);
 
   always_ff(posedge clk_i) begin
     alu_result_w_o <= alu_result_m_i;
