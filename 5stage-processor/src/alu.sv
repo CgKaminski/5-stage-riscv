890043b8a468e9ece6440d5ac6b0dc2af62d9d56
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-23 21:28:16 -0700

    Updated alu wires names to match rest of code

--- 5stage-processor/src/alu.sv
+++ 5stage-processor/src/alu.sv
@@ -1,40 +1,45 @@
 /*
  * ALU Module
  */ 
-module alu(input logic [31:0] A, B,
-          input logic [2:0] alu_control, 
-          output logic [31:0] result,
-          output logic V, C, N, Z);
+module alu(
+  input  logic [31:0] src_a_i,
+  input  logic [31:0] src_b_i,
+  input  logic [2:0]  alu_control_i,
+  output logic [31:0] result_o,
+  output logic        overflow_o,
+  output logic        carry_o,
+  output logic        negative_o,
+  output logic        zero_o);
 
  logic [31:0] and_result, or_result, logic_result, adder_result, slt_result;
  logic [31:0] logic_adder_result;
  logic carry;
 
  // AND + OR case
- assign and_result = A & B;
- assign or_result = A | B;
- assign logic_result = (alu_control[0]) ? or_result : and_result;
+ assign and_result = src_a_i & src_b_i;
+ assign or_result = src_a_i | src_b_i;
+ assign logic_result = (alu_control_i[0]) ? or_result : and_result;
 
  // ADD + SUB case
- assign {carry, adder_result} = (alu_control[0]) ? A - B : A + B;
+ assign {carry, adder_result} = (alu_control_i[0]) ? src_a_i - src_b_i : src_a_i + src_b_i;
 
  // SLT case
- assign slt_result = {31'b0, V ^ adder_result[31]};
+ assign slt_result = {31'b0, overflow_o ^ adder_result[31]};
 
  // Result
- assign logic_adder_result = (alu_control[1]) ? logic_result : adder_result;
- assign result = (alu_control[2]) ? slt_result : logic_adder_result;
+ assign logic_adder_result = (alu_control_i[1]) ? logic_result : adder_result;
+ assign result_o = (alu_control_i[2]) ? slt_result : logic_adder_result;
  
  // Overflow flag
- assign V = (((A[31] == B[31]) && (alu_control[0] == 0)) || ((A[31] != B[31]) && (alu_control[0] == 1))) && (A[31] != adder_result[31]) && ~alu_control[1];
+ assign overflow_o = (((src_a_i[31] == src_b_i[31]) && (alu_control_i[0] == 0)) || ((src_a_i[31] != src_b_i[31]) && (alu_control_i[0] == 1))) && (src_a_i[31] != adder_result[31]) && ~alu_control_i[1];
 
  // Carry flag
- assign C = ~alu_control[1] && ~alu_control[2] && carry;
+ assign carry_o = ~alu_control_i[1] && ~alu_control_i[2] && carry;
 
  // Negative flag
- assign N = result[31];
+ assign negative_o = result_o[31];
 
  // Zero flag
- assign Z = (32'b0 == result);
+ assign zero_o = (32'b0 == result_o);
 
 endmodule
