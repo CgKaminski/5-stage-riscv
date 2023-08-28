05376a2a992d450c1aa66f2b9fee00c748a9e852
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-27 17:01:04 -0700

    Added warning ignore for width trunc

--- 5stage-processor/scripts/top_testbench.sh
+++ 5stage-processor/scripts/top_testbench.sh
@@ -1,3 +1,4 @@
-verilator --cc src/top.sv -Wno-WIDTHEXPAND -Wno-WIDTHTRUNC -Wno-COMBDLY -Wno-STMTDLY -Wno-INITIALDLY --no-timing --exe top_testbench.cpp
+## verilator  --cc -Wall src/top.sv -Wno-WIDTHEXPAND -Wno-WIDTHTRUNC -Wno-COMBDLY -Wno-STMTDLY -Wno-INITIALDLY --no-timing --exe tests/top_testbench.cpp
+verilator  --cc -Wall src/top.sv -Wno-WIDTHEXPAND -Wno-UNUSEDSIGNAL -Wno-WIDTHTRUNC --trace --exe tests/top_testbench.cpp
 make -C obj_dir -f Vtop.mk
-obj_dir/top
+obj_dir/Vtop
