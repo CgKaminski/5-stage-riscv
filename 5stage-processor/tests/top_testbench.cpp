0c99c2bb2d45c4b6ee17e1056c51e984a93bfab1
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-27 17:07:19 -0700

    Updated top test

--- 5stage-processor/tests/top_testbench.cpp
+++ 5stage-processor/tests/top_testbench.cpp
@@ -1,50 +1,43 @@
 #include <verilated.h>
-#include "Vprocessor.h"
+#include "Vtop.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
 
-    Vprocessor* top = new Vprocessor;
+    Vtop* top = new Vtop;
 
-    top->clk_i = 0;
-    top->reset_i = 1;
+    std::cout << "Simulation started\n" << std::endl;
 
-    top->eval();
-    top->reset = 0;
-    top->eval();
-
-    std::cout << "Simulation started" << std::endl;
-
-    for (int cycle = 0; cycle < 100; ++cycle) {
-        top->clk = 1;
-        top->eval();
-        top->clk = 0;
-        top->eval();
-    }
-
-    for (int cycle = 0; cycle < 200; ++cycle) {
-        top->clk = 1;
+    top->reset_i = 0;
+    std::cout << "Simulation running" << std::endl;
+    for (int cycle = 0; cycle < 30 ; ++cycle) {
+        std::cout << "Cycle: " << cycle << std::endl;
+        top->clk_i = 1;
         top->eval();
 
-        if (top->memwrite) {
-            if (top->data_adr_o == 84 && top->write_data_o == 7) {
-                printf("Simulation succeeded\n");
+        if (top->mem_write_o) {
+            if (top->data_adr_o == 100 && top->write_data_o == 25) {
+                printf("Test succeeded\n");
                 std::cout << "dataadr: " << top->data_adr_o << std::endl;
                 std::cout << "writedata: " << top->write_data_o << std::endl;
+                std::cout << "memwrite: " << top->mem_write_o << std::endl;
                 break;
-            } else if (top->data_adr_o != 80) {
-                printf("Simulation failed\n");
+            } else if (top->data_adr_o != 96) {
+                printf("Test failed\n");
                 std::cout << "dataadr: " << top->data_adr_o << std::endl;
                 std::cout << "writedata: " << top->write_data_o << std::endl;
+                std::cout << "memwrite: " << top->mem_write_o << std::endl;
                 break;
             }
         }
+        
 
-        top->clk = 0;
+        top->clk_i = 0;
         top->eval();
     }
 
     delete top;
+    std::cout << "Simulation ended\n" << std::endl;
     return 0;
 }
