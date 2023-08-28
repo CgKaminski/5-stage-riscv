3a5db0a8937f658262790af503fc0fb46296d229
CgKaminski <cameron.g.kaminski@gmail.com>
2023-08-27 17:02:24 -0700

    Refactored

--- 5stage-processor/src/dmem.sv
+++ 5stage-processor/src/dmem.sv
@@ -5,13 +5,13 @@ module dmem(
   input  logic        clk_i,
   input  logic        we_i,
   input  logic [31:0] a_i,
-  input  logic        wd_i,
+  input  logic [31:0] wd_i,
   output logic [31:0] rd_o);
 
   logic [31:0] ram[63:0];
-  assign rd = ram[a[31:2]];
+  assign rd_o = ram[a_i[31:2]];
 
-  always_ff (posedge clk_i) begin
+  always_ff @(posedge clk_i) begin
     if (we_i) 
       ram[a_i[31:2]] <= wd_i;
   end
